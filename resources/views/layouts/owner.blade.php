a4e812c30402efe98956338ffdc513cf524add8b
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-19 12:13:14 +0330

    use role specific asset bundle

--- resources/views/layouts/owner.blade.php
+++ resources/views/layouts/owner.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'مدیریت آموزشگاه | شیخان')</title>
-    @vite(['resources/css/academy-teacher.css', 'resources/js/academy-teacher.js'])
+    @vite(['resources/css/owner.css', 'resources/js/owner.js'])
     @stack('styles')
 </head>
 <body class="owner-shell">
