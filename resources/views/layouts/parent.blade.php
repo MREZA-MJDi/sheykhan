aa4e4912675e886c274401b7cbcb0bac2dab129c
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-19 11:23:40 +0330

    separate parent.blade.php layout

--- resources/views/layouts/parent.blade.php
+++ resources/views/layouts/parent.blade.php
@@ -4,32 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>@yield('title', 'پنل والدین | شیخان')</title>
-
-    @vite([
-        'resources/css/student-parent.css',
-        'resources/js/student-parent.js',
-    ])
-
+    <title>@yield('title', 'پنل والد | شیخان')</title>
+    @vite(['resources/css/parent.css', 'resources/js/parent.js'])
     @stack('styles')
 </head>
+<body class="parent-shell">
+    <div class="role-layout">
+        <x-navigation.parent-sidebar />
 
-<body class="learner-shell">
-    <div class="flex min-h-screen">
-        <x-navigation.sidebar title="پنل والدین">
-            @yield('sidebar')
-        </x-navigation.sidebar>
-
-        <div class="min-w-0 flex-1">
-            <x-navigation.panel-topbar title="@yield('header-title', 'پنل والدین')" />
-
-            <main class="min-w-0 p-4 sm:p-6 lg:p-8 learner-page-enter">
-                @yield('content')
-            </main>
+        <div class="role-content">
+            <x-navigation.panel-topbar role="parent" title="@yield('header-title', 'پنل والد')" />
+            <main class="role-main">@yield('content')</main>
         </div>
     </div>
-
     @stack('scripts')
 </body>
 </html>
