917bf7b4537d5f1405de8297e277bbefa4baab12
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-19 12:10:08 +0330

    use shared role asset bundles

--- resources/views/layouts/teacher.blade.php
+++ resources/views/layouts/teacher.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'پنل استاد | شیخان')</title>
-    @vite(['resources/css/teacher.css', 'resources/js/teacher.js'])
+    @vite(['resources/css/academy-teacher.css', 'resources/js/academy-teacher.js'])
     @stack('styles')
 </head>
 <body class="teacher-shell">
