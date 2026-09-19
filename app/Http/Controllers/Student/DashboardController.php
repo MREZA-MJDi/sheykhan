6ab3b79d7c8dc4c7035c169c33a6cdc50186f9ec
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-19 11:22:24 +0330

    wire DashboardController.php to dashboard service

--- app/Http/Controllers/Student/DashboardController.php
+++ app/Http/Controllers/Student/DashboardController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
+use App\Services\StudentDashboardService;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
-    public function __invoke(): View
+    public function __invoke(StudentDashboardService $dashboard): View
     {
-        return view('student.dashboard');
+        return view('student.dashboard', $dashboard->build(request()->user()));
     }
 }
