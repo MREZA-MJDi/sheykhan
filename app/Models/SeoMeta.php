5aab0cec7541dc425c46fd076fa65324e4b4df37
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-19 09:51:33 +0330

    fix(domain): align SeoMeta.php relationships

--- app/Models/SeoMeta.php
+++ app/Models/SeoMeta.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -9,13 +8,8 @@ use Illuminate\Database\Eloquent\Relations\MorphTo;
 class SeoMeta extends Model
 {
     use HasFactory;
-
-    protected $fillable = [
-        'seoable_type','seoable_id','title','description','keywords','canonical_url',
-        'robots','og_title','og_description','og_image_url','schema_json'
-    ];
-
-    protected $casts = ['schema_json' => 'array'];
-
+    protected $table='seo_metas';
+    protected $fillable=['seoable_type','seoable_id','title','description','keywords','canonical_url','robots','og_title','og_description','og_image_url','schema_json'];
+    protected $casts=['schema_json'=>'array'];
     public function seoable(): MorphTo { return $this->morphTo(); }
 }
