ab60f91d41268c11bbdca5646a5e0b3c468c808a
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-19 12:10:57 +0330

    remove split role asset file

--- resources/js/owner.js
+++
@@ -1,6 +0,0 @@
-import { bootPanel } from './panel-base.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    bootPanel();
-    document.documentElement.classList.add('owner-ready');
-});
