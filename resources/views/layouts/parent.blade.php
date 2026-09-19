c838279d76b84820ca1ad8518b90734e578a8f3d
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-19 12:13:28 +0330

    use role specific asset bundle

--- resources/views/layouts/parent.blade.php
+++ resources/views/layouts/parent.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'پنل والد | شیخان')</title>
-    @vite(['resources/css/student-parent.css', 'resources/js/student-parent.js'])
+    @vite(['resources/css/parent.css', 'resources/js/parent.js'])
     @stack('styles')
 </head>
 <body class="parent-shell">
