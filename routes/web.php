0a6c14f3065571d49825e37724d09547f3de6ca8
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-24 20:53:12 +0330

    fix(owner): restore teacher schedule controller namespace

--- routes/web.php
+++ routes/web.php
@@ -92,7 +92,7 @@ Route::middleware(['auth','role:teacher'])->prefix('teacher')->name('teacher.')-
     Route::post('/classrooms', [TeacherClassroomController::class, 'store'])->name('classrooms.store');
     Route::get('/classrooms/{classroom}/attendance', [TeacherAttendanceController::class, 'edit'])->name('classrooms.attendance.edit');
     Route::post('/classrooms/{classroom}/attendance', [TeacherAttendanceController::class, 'store'])->name('classrooms.attendance.store');
-    Route::get('/schedule', [App\Http\ControllersTeacherScheduleController::class, 'index'])->name('schedule.index');
+    Route::get('/schedule', [App\Http\Controllers\Teacher\ScheduleController::class, 'index'])->name('schedule.index');
     Route::post('/schedule', [App\Http\ControllersTeacherScheduleController::class, 'store'])->name('schedule.store');
     Route::get('/students', fn (\App\Services\TeacherWorkspaceService $workspace) => view('teacher.students.index', ['students' => $workspace->students(request()->user())]))->name('students.index');
     Route::get('/assignments', [TeacherAssignmentController::class, 'index'])->name('assignments.index');
