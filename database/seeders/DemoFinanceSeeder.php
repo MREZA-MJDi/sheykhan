64c911059ca0644dd294fd11800a1876285a7153
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-24 20:53:10 +0330

    fix(owner): normalize finance seeder namespaces

--- database/seeders/DemoFinanceSeeder.php
+++ database/seeders/DemoFinanceSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\\CourseEnrollment;
-use App\Models\\FinancialTransaction;
-use Illuminate\Database\\Seeder;
+use App\Models\CourseEnrollment;
+use App\Models\FinancialTransaction;
+use Illuminate\Database\Seeder;
 
 class DemoFinanceSeeder extends Seeder
 {
