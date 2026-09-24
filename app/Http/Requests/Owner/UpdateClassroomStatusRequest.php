9feeb2dce6aea4cef3d7dd94727b5bb5931c1ee7
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-24 20:53:05 +0330

    fix(owner): normalize classroom request namespaces

--- app/Http/Requests/Owner/UpdateClassroomStatusRequest.php
+++ app/Http/Requests/Owner/UpdateClassroomStatusRequest.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Requests\Owner;
 
-use Illuminate\Foundation\Http\\FormRequest;
-use Illuminate\Validation\\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateClassroomStatusRequest extends FormRequest
 {
