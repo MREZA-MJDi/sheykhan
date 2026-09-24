5f63cb172245eff99bc4dc7b7057638092368c4b
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-24 23:58:17 +0330

    register active teacher membership middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\EnsurePermission;
 use App\Http\Middleware\EnsureRole;
+use App\Http\Middleware\EnsureActiveTeacherMembership;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => EnsureRole::class,
             'permission' => EnsurePermission::class,
+            'active-teacher' => EnsureActiveTeacherMembership::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
