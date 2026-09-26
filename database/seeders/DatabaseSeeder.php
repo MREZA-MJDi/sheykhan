91f161f306c8e5e25782f4af64c9a3eed159c3cb
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-26 10:32:20 +0330

    Use production seeders only

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,19 +1,13 @@
 <?php
-
 namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     public function run(): void
     {
         $this->call([
             RolePermissionSeeder::class,
-            DemoContentSeeder::class,
+            ReferenceDataSeeder::class,
         ]);
     }
 }
