6496d9072e93c1a43e3108818a9898417af4d7b6
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-26 10:54:09 +0330

    refactor: orchestrate domain seeders explicitly

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,17 +1,22 @@
 <?php
-
 namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
-    public function run(): void
-    {
-        $this->call([
-            RolePermissionSeeder::class,
-            ReferenceDataSeeder::class,
-            PlatformDataSeeder::class,
-        ]);
-    }
+ public function run(): void
+ {
+  $this->call([
+   RolePermissionSeeder::class,
+   AcademicReferenceSeeder::class,
+   AcademyFoundationSeeder::class,
+   LearningSeeder::class,
+   CommerceSeeder::class,
+   LegalSeeder::class,
+   PublicContentSeeder::class,
+   BlogSeeder::class,
+   SystemSeeder::class,
+   OnboardingSeeder::class,
+   AuditSeeder::class,
+  ]);
+ }
 }
